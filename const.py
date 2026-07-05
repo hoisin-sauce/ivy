74cc51ccec375487e4a4d7275e24a0a1c0d83412
hoisin-sauce <97987829+hoisin-sauce@users.noreply.github.com>
2026-07-05 22:18:34 +0100

    transfer from laptop

---
+++ const.py
@@ -0,0 +1,4 @@
+import typing
+
+BASIC_TYPES = {str, int, bool, float}
+BASIC_TYPE_MAPPINGS = ...
--- datalib.py
+++ datalib.py
@@ -2,7 +2,7 @@ import typing
 import types
 import utils
 import queue
-
+import const
 
 class Database:
     def __init__(self, modules: list[types.ModuleType]):
@@ -19,24 +19,50 @@ def get_classes_from_module(module: types.ModuleType) -> list[typing.Type]:
     return classes
 
 
-def get_dependencies(object_type: typing.Type) -> list[typing.Type]:
-    objects = []
+def get_dependencies(object_type: typing.Type) -> dict[str, list[typing.Type]]:
+    pairings = []
     q = queue.Queue()
     [q.put(i) for i in typing.get_type_hints(object_type).items()]
     while not q.empty():
         s, x = q.get()
         if repr(type(x)) == '<class \'type\'>':
-            objects.append(x)
+            pairings.append((s,x))
         else:
-            print(type(x))
             [q.put((s, i)) for i in x.__args__]
 
-    print(objects)
+    fields : dict[str, list[typing.Type]]= dict()
+    for key, value in pairings:
+        if key not in fields:
+            fields[key] = [value]
+        else:
+            fields[key].append(value)
+
+    return fields
+
+def get_table_contents_from_map(mapping: dict[str, list[typing.Type]]) -> str:
+    strings : list[str] = list()
+    for key, value in mapping:
+        values = len(value)
+
+        assert values != 0
+
+        optional = typing.
+
+        if values == 1:
+            if value in const.BASIC_TYPES:
+                # add raw field
+                ...
+            else:
+                # add id field
+                ...
+        else:
+            # add the class switch selector thing
+            ...
+
 
 def main():
     import modeldata
-    print(get_classes_from_module(modeldata))
-    get_dependencies(modeldata.ModelResponse)
+    print(get_dependencies(modeldata.ModelResponse))
 
 if __name__ == "__main__":
     main()
