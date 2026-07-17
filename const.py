643336925b49ea2e16a445c60d347c7513998c49
hoisin-sauce <97987829+hoisin-sauce@users.noreply.github.com>
2026-07-17 18:27:47 +0100

    Before attempted refactoring of datalib

--- const.py
+++ const.py
@@ -1,6 +1,23 @@
 import enum
 import typing
 
+class SQLFields(enum.Enum):
+    required = "NOT NULL"
+    primary = "PRIMARY KEY"
+    autoincrement = "AUTO_INCREMENT"
+    foreign_key = "FOREIGN KEY ({local_column_name}) REFERENCES {table_name} ({foreign_column_name})"
+    integer = "INTEGER"
+    string = "TEXT"
+    real = "REAL"
+    null = "NULL"
+    blob = "BLOB"
+    json = "JSON"
+    in_range = "CHECK ({local_column_name} >= {minimum} AND {local_column_name} <= {maximum})"
+    unique = "UNIQUE ({local_comma_separated_columns})"
+    unique_with_primary = "UNIQUE ({local_comma_separated_columns})"
+
+
+
 BASIC_TYPES = {str, int, bool, float, type(None), Ellipsis}
 BASIC_TYPE_MAPPINGS = ...
 
@@ -8,3 +25,7 @@ NONE_TYPE = type(None)
 
 TYPE_EXCEPTIONS = {...}
 INHERIT_EXCEPTIONS = (enum.Enum,)
+
+class SQLFieldDefaults(enum.Enum):
+    PRIMARY_KEY_CONSTRUCTOR = [SQLFields.integer, SQLFields.primary, SQLFields.autoincrement]
+    ORDERING_FIELD = [SQLFields.integer, SQLFields.required, SQLFields.unique_with_primary]
--- datalib.py
+++ datalib.py
@@ -1,12 +1,11 @@
 import enum
-import time
 import typing
 import types
 import utils
 import queue
 import const
-from utils import flatten_to_list
-from collections.abc import Iterable
+from collections.abc import Iterable, Sequence
+from const import SQLFields, SQLFieldDefaults
 
 
 class Database:
@@ -14,7 +13,7 @@ class Database:
         classes: list[typing.Type] = \
             utils.flatten_to_list([get_classes_from_module(i) for i in modules])
 
-        dependency_map: dict[typing.Type, set[typing.Type]] = {}
+        self.dependency_map: dict[typing.Type, set[typing.Type]] = {}
         resolved_dependencies: set[typing.Type] = const.BASIC_TYPES
         resolution_order: list[typing.Type] = list()
 
@@ -24,14 +23,14 @@ class Database:
             # TODO resolve circluar / optimise control flow for large N
             current_type = classes.pop(0)
 
-            if current_type not in dependency_map:
+            if current_type not in self.dependency_map:
 
                 current_field_dependencies = get_dependencies(current_type)
-                dependency_map[current_type] = set(utils.flatten_to_list(current_field_dependencies.values()))
+                self.dependency_map[current_type] = set(utils.flatten_to_list(current_field_dependencies.values()))
 
-            if not dependency_map[current_type].issubset(resolved_dependencies):
-                print(dependency_map[current_type], resolved_dependencies)
-                [classes.append(i) for i in dependency_map[current_type]-resolved_dependencies if i not in classes]
+            if not self.dependency_map[current_type].issubset(resolved_dependencies):
+                print(self.dependency_map[current_type], resolved_dependencies)
+                [classes.append(i) for i in self.dependency_map[current_type]-resolved_dependencies if i not in classes]
                 classes.append(current_type)
                 continue
 
@@ -40,6 +39,7 @@ class Database:
             resolved_dependencies.add(current_type)
 
         print(resolution_order)
+        self.resolved_classes = set(resolution_order)
 
 
 def get_classes_from_module(module: types.ModuleType) -> list[typing.Type]:
@@ -75,24 +75,72 @@ def get_dependencies(object_type: typing.Type) -> dict[str, list[typing.Type]]:
 
     return fields
 
-def get_table_contents_from_type(object_type: typing.Type) -> str:
+def get_table_contents_from_type(self, object_type: typing.Type) -> str:
+    pre_tables: list[str] = list()
+    table_name = f"{object_type.__module__}_{object_type.__name__}"
+    subquery = f"CREATE TABLE {table_name} (\n"
+    table_links: dict[str, str] = {}
+
     if issubclass(object_type, enum.Enum):
         # create switch number to different fixed values?
-        ...
-        return
+        # create a table with fixed rows in it pointing to the current values
+        # i.e. as a lookup table
+        # then we store the actual values
 
-    prior_tables: list[str] = list()
-    subquery = f"CREATE TABLE {object_type.__module__}_{object_type.__name__} (\n"
-    table_links: dict[str, str] = {}
-    for name, type in typing.get_type_hints(object_type).items():
+        lookup_table_name = table_name + "_enum_"
+
+        lookup_table_fields = {
+            lookup_table_name + "_id": SQLFieldDefaults.PRIMARY_KEY_CONSTRUCTOR,
+
+        }
+
+        # how do we manage to deal with different typing?
+        # should we also handle custom objects within enums?
+        if len(dependencies := self.dependency_map[object_type]) == 1:  # foreign types
+            # we can just store the value in one database
+            dependencies: set[typing.Type]
+
+            enum_field_type = dependencies.pop()
+            assert enum_field_type in const.BASIC_TYPES, "Enums should only hold basic values"
+        else:
+            # TODO multi table switching
+            raise NotImplemented("Enums can only hold one datatype")
+            # we need to manage storing multiple different datatypes here
+            ...
+        return ""
+
+
+
+    for field_name, type in typing.get_type_hints(object_type).items():
         # should be abstracted to another method
         # would make the random insertion of union types much easier
         if issubclass(type, Iterable):
             # handle iterable code
+
+            # create a table linking to current table id and iterator id
+            # iterator id is a table holding the contents
+            # e.g. for a list[str]
+            # we create table fieldname
+            # with tablename id and resolve the str field to str
+
+            # how do we avoid name collisions here
+            # make the iterator field depend on something like
+
+            iterator_table_name = table_name + "_iter_" + field_name
+
+            iterator_table_fields = {
+                iterator_table_name + "_id": SQLFieldDefaults.PRIMARY_KEY_CONSTRUCTOR
+            }
+
+            if issubclass(type, Sequence):
+                iterator_table_fields["position"] = SQLFieldDefaults.ORDERING_FIELD
+
+            # We now need to point to the datatype inside the iterable object
             ...
 
         if issubclass(type, enum.Enum):
-            # handle subclass code
+
+            ...
 
 
 def main():
@@ -101,3 +149,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+# TODO manage migration/ changing datastructures, e.g. matching like tables
