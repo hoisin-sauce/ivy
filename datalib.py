acdf05fde0bfce8e609b0350b02527f4470b069b
hoisin-sauce <97987829+hoisin-sauce@users.noreply.github.com>
2026-07-20 07:03:57 +0100

    Completed refactor of datalib.py

--- datalib.py
+++ datalib.py
@@ -1,153 +1,73 @@
-import enum
-import typing
 import types
+import typing
+from dataclasses import dataclass
+from typing import Any, Type, Mapping, Self
+from types import ModuleType, UnionType, GenericAlias
+from collections.abc import Iterable
 import utils
-import queue
-import const
-from collections.abc import Iterable, Sequence
-from const import SQLFields, SQLFieldDefaults
-
+import enum
 
 class Database:
-    def __init__(self, modules: list[types.ModuleType]):
-        classes: list[typing.Type] = \
-            utils.flatten_to_list([get_classes_from_module(i) for i in modules])
+    def __init__(self, modules: Iterable[ModuleType]) -> None:
+        types_by_module: Iterable[Iterable[Type]] = list(map(get_types_in_module, modules))
+        self.types = utils.flatten_to_set(types_by_module)
+        self.datatype_map: dict[Type, DataType] = dict(map(DataType.build_datatype_from_type, self.types))
+        self._introduce_backwards_dependencies()
 
-        self.dependency_map: dict[typing.Type, set[typing.Type]] = {}
-        resolved_dependencies: set[typing.Type] = const.BASIC_TYPES
-        resolution_order: list[typing.Type] = list()
+    def _introduce_backwards_dependencies(self):
+        for datatype, datatype_information in self.datatype_map.items():
+            map(lambda x: self.datatype_map[x].depended_by.append(datatype), datatype_information.dependencies)
 
-        print(classes)
+@dataclass
+class DataType:
+    class_type: type
+    dependencies: list[type]
+    remaining_dependencies: list[type]
+    depended_by: list[type]
 
-        while len(classes) != 0:
-            # TODO resolve circluar / optimise control flow for large N
-            current_type = classes.pop(0)
+    @staticmethod
+    def build_datatype_from_type(class_type: Type) -> (Type, 'DataType'):
+        dependencies: list[Type] = list(get_immediate_dependencies(class_type))
+        datatype: DataType = DataType(class_type, dependencies, dependencies, list())
+        return class_type, datatype
 
-            if current_type not in self.dependency_map:
+def is_type(obj: Any):
+    return isinstance(obj, type)
 
-                current_field_dependencies = get_dependencies(current_type)
-                self.dependency_map[current_type] = set(utils.flatten_to_list(current_field_dependencies.values()))
+def get_types_in_module(module: ModuleType) -> list[Type]:
+    return [_type for i in dir(module) if is_type(_type := getattr(module, i))]
 
-            if not self.dependency_map[current_type].issubset(resolved_dependencies):
-                print(self.dependency_map[current_type], resolved_dependencies)
-                [classes.append(i) for i in self.dependency_map[current_type]-resolved_dependencies if i not in classes]
-                classes.append(current_type)
-                continue
+def get_immediate_dependencies(class_type: Type) -> Iterable[Type]:
+    # We handle enums by checking the type of their child elements
+    # This is incase an enum points to a newly defined class and as such
+    # That table needs to be declared first before we can point to it
+    if issubclass(class_type, enum.Enum):
+        return {type(v.value) for v in class_type._member_map_.values()}
 
-            # we can now actually place it in order
-            resolution_order.append(current_type)
-            resolved_dependencies.add(current_type)
-
-        print(resolution_order)
-        self.resolved_classes = set(resolution_order)
-
-
-def get_classes_from_module(module: types.ModuleType) -> list[typing.Type]:
-    classes = []
-    for object_name in utils.public_dir(module):
-        obj = getattr(module, object_name)
-        if repr(type(obj)) == '<class \'type\'>':
-            classes.append(obj)
-
-    return classes
-
-def get_dependencies(object_type: typing.Type) -> dict[str, list[typing.Type]]:
-    pairings = []
-    q = queue.Queue()
-    [q.put(i) for i in typing.get_type_hints(object_type).items()]
-    if issubclass(object_type, enum.Enum):
-        [q.put((k, type(v.value))) for k, v in object_type._member_map_.items()]
-    while not q.empty():
-        s, x = q.get()
-        if isinstance(x, type):
-            pairings.append((s,x))
-        elif x in const.TYPE_EXCEPTIONS or utils.safe_is_subclass(x, const.INHERIT_EXCEPTIONS):
-            pairings.append((s,x))
-        else:
-            [q.put((s, i)) for i in x.__args__]
-
-    fields : dict[str, list[typing.Type]]= dict()
-    for key, value in pairings:
-        if key not in fields:
-            fields[key] = [value]
-        else:
-            fields[key].append(value)
-
-    return fields
-
-def get_table_contents_from_type(self, object_type: typing.Type) -> str:
-    pre_tables: list[str] = list()
-    table_name = f"{object_type.__module__}_{object_type.__name__}"
-    subquery = f"CREATE TABLE {table_name} (\n"
-    table_links: dict[str, str] = {}
-
-    if issubclass(object_type, enum.Enum):
-        # create switch number to different fixed values?
-        # create a table with fixed rows in it pointing to the current values
-        # i.e. as a lookup table
-        # then we store the actual values
-
-        lookup_table_name = table_name + "_enum_"
-
-        lookup_table_fields = {
-            lookup_table_name + "_id": SQLFieldDefaults.PRIMARY_KEY_CONSTRUCTOR,
-
-        }
-
-        # how do we manage to deal with different typing?
-        # should we also handle custom objects within enums?
-        if len(dependencies := self.dependency_map[object_type]) == 1:  # foreign types
-            # we can just store the value in one database
-            dependencies: set[typing.Type]
-
-            enum_field_type = dependencies.pop()
-            assert enum_field_type in const.BASIC_TYPES, "Enums should only hold basic values"
-        else:
-            # TODO multi table switching
-            raise NotImplemented("Enums can only hold one datatype")
-            # we need to manage storing multiple different datatypes here
-            ...
-        return ""
+    type_hints: Mapping[str, Type | UnionType | GenericAlias] = typing.get_type_hints(class_type)
+    argument_types: Iterable[Type | UnionType | GenericAlias] = type_hints.values()
+    return utils.flatten_to_set(map(resolve_type, argument_types))
 
 
+def resolve_type(class_type: typing.Union[types.UnionType, types.GenericAlias, Type]) -> Iterable[Type]:
+    match meta_type := type(class_type):
 
-    for field_name, type in typing.get_type_hints(object_type).items():
-        # should be abstracted to another method
-        # would make the random insertion of union types much easier
-        if issubclass(type, Iterable):
-            # handle iterable code
+        case types.UnionType:
+            return utils.flatten_to_set(map(resolve_type, resolve_union_type(class_type)))
 
-            # create a table linking to current table id and iterator id
-            # iterator id is a table holding the contents
-            # e.g. for a list[str]
-            # we create table fieldname
-            # with tablename id and resolve the str field to str
+        case types.GenericAlias:
+            return utils.flatten_to_set(map(resolve_type, resolve_generic_alias(class_type)))
 
-            # how do we avoid name collisions here
-            # make the iterator field depend on something like
+        case _:
+            if meta_type == type:
+                return (class_type,)
+            raise NotImplementedError(f"Type {meta_type} has not been implemented")
 
-            iterator_table_name = table_name + "_iter_" + field_name
+def resolve_union_type(class_type: types.UnionType) -> Iterable[Type]:
+    return typing.get_args(class_type)
 
-            iterator_table_fields = {
-                iterator_table_name + "_id": SQLFieldDefaults.PRIMARY_KEY_CONSTRUCTOR
-            }
+def resolve_generic_alias(class_type: types.GenericAlias) -> Iterable[Type]:
+    if isinstance(alias := typing.get_origin(class_type), Iterable):
+        return typing.get_args(class_type)
 
-            if issubclass(type, Sequence):
-                iterator_table_fields["position"] = SQLFieldDefaults.ORDERING_FIELD
-
-            # We now need to point to the datatype inside the iterable object
-            ...
-
-        if issubclass(type, enum.Enum):
-
-            ...
-
-
-def main():
-    import modeldata
-    get_table_contents_from_type(modeldata.ModelResponse)
-
-if __name__ == "__main__":
-    main()
-
-# TODO manage migration/ changing datastructures, e.g. matching like tables
+    raise NotImplementedError(f"Type {alias} has not been implemented")
